cf1b268594be9fbc131e49f83fe7e785ed85d87b
Oscar <oscaritoparapito225@mail.ru>
2025-05-29 19:49:59 +0600

    first commit

---
+++ dist/js/app.js
@@ -0,0 +1,20 @@
+(() => {
+    "use strict";
+    let bodyLockStatus = true;
+    function menuInit() {
+        if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
+            if (bodyLockStatus && e.target.closest(".icon-menu")) document.documentElement.classList.toggle("menu-open");
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    window["FLS"] = false;
+    menuInit();
+})();
